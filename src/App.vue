113339fcd37428340807df56b4aac966f83bc936
Abrar Mohi Shafee <abrarmohi@yahoo.com>
2022-01-22 15:42:00 +0100

    git init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+// https://github.com/vueuse/head
+// you can use this to manipulate the document head in any components,
+// they will be rendered correctly in the html results with vite-ssg
+useHead({
+  title: '{abrar.codes}',
+  meta: [
+    { name: 'description', content: 'Just another WIP' },
+  ],
+})
+</script>
+
+<template>
+  <router-view />
+</template>
